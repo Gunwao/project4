a69423867b038161f9ade926ec0246e1d3e9426e
gunwao <olagunwao@gmail.com>
2022-10-11 19:39:47 -0500

    push backend

--- backend.tf
+++ backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "remote" {}
+  backend "azurerm" {
+    resource_group_name  = "tfstateRG01"
+    storage_account_name = "tfstate01back134"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
